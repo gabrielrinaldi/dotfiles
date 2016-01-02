2ded21ef4b0faa943fcec7013edeca6bf11ab6cb
Gabriel Rinaldi <gabriel_rinaldi@mac.com>
2016-01-02 09:02:05 -0800

    Added Redis alias

--- oh-my-zsh/plugins/aliases/aliases.plugin.zsh
+++ oh-my-zsh/plugins/aliases/aliases.plugin.zsh
@@ -11,6 +11,9 @@ alias hosts="sudo vim /etc/hosts"
 alias pg_start='pg_ctl -D /usr/local/var/postgres -l logfile start'
 alias pg_stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 
+# Redis
+alias rd_start='redis-server /usr/local/etc/redis.conf'
+
 # Bower
 alias bower='noglob bower'
 
