cfd5a7fa066b2c90bfded3c0951d2ad127bf3b14
Gabriel Rinaldi <gabrielrinaldi@users.noreply.github.com>
2022-05-01 18:40:03 -0600

    [chore]: cleanup aliases

--- aliases.zsh
+++ aliases.zsh
@@ -5,5 +5,4 @@ alias grep='grep --color=auto'                # colorize `grep` output
 alias less='less -R'
 alias g='git'
 
-alias cal='gcal --starting-day=1'             # print simple calendar for current month
-alias weather='curl v2.wttr.in'               # print weather for current location (https://github.com/chubin/wttr.in)
+alias weather='curl wttr.in/~Castle+Rock'               # print weather for current location (https://github.com/chubin/wttr.in)
