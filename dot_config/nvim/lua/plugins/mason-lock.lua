fdcdd2ad8b79bc12aaaeb22d3d05eedb05b6c43d
Gabriel Rinaldi <gabrielrinaldi@users.noreply.github.com>
2025-02-22 14:02:03 -0700

    Update .config/nvim/lua/plugins/mason-lock.lua

--- dot_config/nvim/lua/plugins/mason-lock.lua
+++ dot_config/nvim/lua/plugins/mason-lock.lua
@@ -2,5 +2,10 @@ return {
   -- Mason lock file support
   {
     "zapling/mason-lock.nvim",
+    init = function()
+      require("mason-lock").setup({
+        lockfile_path = vim.fn.stdpath("config") .. "/mason-lock.json", -- (default)
+      })
+    end,
   },
 }
