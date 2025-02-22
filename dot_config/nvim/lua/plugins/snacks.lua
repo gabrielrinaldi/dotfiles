03821d437bf76d3cd092bc08ec4ade912b5a1c2e
Gabriel Rinaldi <gabrielrinaldi@users.noreply.github.com>
2025-02-22 13:57:19 -0700

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -10,18 +10,11 @@ return {
       dashboard = {
         enabled = true,
         sections = {
-          {
-            pane = 2,
-            section = "health",
-            height = 5,
-            padding = 1,
-          },
           { section = "header" },
-          { section = "keys", gap = 1, padding = 1 },
-          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           {
-            pane = 2,
             icon = " ",
             title = "Git Status",
             section = "terminal",
@@ -32,7 +25,7 @@ return {
             height = 5,
             padding = 1,
             ttl = 5 * 60,
-            indent = 3,
+            indent = 2,
           },
           { section = "startup" },
         },
