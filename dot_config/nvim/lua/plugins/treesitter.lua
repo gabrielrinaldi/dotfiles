0b7840b8578f0e08aeb3d64f3840af99cf784d2c
Gabriel Rinaldi <gabrielrinaldi@users.noreply.github.com>
2025-02-23 01:15:32 -0700

    chore: update .chezmoidata/packages.yaml chore: update dot_config/nvim/lua/plugins/treesitter.lua feat: add empty_dot_tmux.conf

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,13 @@ return {
   {
     -- Treesitter additional file types
     "nvim-treesitter/nvim-treesitter",
+    init = function()
+      require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+        local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+        local filename = vim.fn.fnamemodify(filepath, ":t")
+        return string.match(filename, ".*mise.*%.toml$")
+      end, { force = true, all = false })
+    end,
     opts = function(_, _)
       vim.filetype.add({
         extension = { jbuilder = "ruby" },
