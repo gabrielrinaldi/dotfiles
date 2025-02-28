882a5bdefde5a4f71d1bfc90c8010a328603409e
Gabriel Rinaldi <gabriel.rinaldi@glssolutions.us>
2025-02-27 17:21:05 -0700

    chore: remove mini move fix

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Support mini.move
--- vim.keymap.del({ "n", "i", "v" }, "<A-j>")
--- vim.keymap.del({ "n", "i", "v" }, "<A-k>")
