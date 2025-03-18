13b01cc39252e9aa9d6a8e4cf791d2778ce24db9
Gabriel Rinaldi <gabriel.rinaldi@glssolutions.us>
2025-03-17 23:15:29 -0600

    fix: eza cannot list specified directories

--- dot_config/fish/functions/ll.fish
+++ dot_config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ls --description 'List directory contents'
+function ll --description 'List directory contents'
     eza -labh --icons auto --group-directories-first --git --time-style relative --no-permissions --no-user
 end
