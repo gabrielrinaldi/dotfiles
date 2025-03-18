a748c91f14bd185ac2ec4f25adbbf6d612a2e32a
Gabriel Rinaldi <gabriel.rinaldi@glssolutions.us>
2025-03-17 23:15:53 -0600

    chore: add git abbreviations

--- dot_config/fish/conf.d/abbr.fish
+++ dot_config/fish/conf.d/abbr.fish
@@ -1,3 +1,25 @@
 if status is-interactive
-    abbr --add gst git status
+    # Homebrew
+    abbr --add brewup 'brew upgrade'
+
+    # Git
+    abbr --add ga 'git add'
+    abbr --add gaa 'git add --all'
+    abbr --add gb 'git branch'
+    abbr --add gc 'git commit'
+    abbr --add gc! 'git commit --verbose --amend'
+    abbr --add gcb 'git checkout -b'
+    abbr --add gcn! 'git commit --verbose --no-edit --amend'
+    abbr --add gco 'git checkout'
+    abbr --add gg lazygit
+    abbr --add ggpush 'git push origin "$(git_current_branch)"'
+    abbr --add ghpr 'gh pr create --fill'
+    abbr --add gpr 'git pull --rebase --autostash'
+    abbr --add gst 'git status'
+    abbr --add gsth 'git stash --all'
+    abbr --add gstp 'git stash pop'
+
+    # Paths
+    abbr --add config '~/.config/'
+    abbr --add local '~/.local/'
 end
