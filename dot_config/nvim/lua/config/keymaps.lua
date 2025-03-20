74b5797dbea1dcb608147d72c7debe5c0c3d3113
Gabriel Rinaldi <gabriel.rinaldi@glssolutions.us>
2025-03-19 21:36:14 -0600

    chore: improve avant integration with lazyvim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-local wk = require("which-key")
-wk.add({
-  { "<leader>a", group = "Avante", icon = "󰚩" },
-})
--- dot_config/nvim/lua/plugins/avante.lua
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -2,8 +2,47 @@ return {
   -- Setup Avante as AI option
   {
     "yetone/avante.nvim",
+    lazy = true,
     event = "VeryLazy",
     version = false,
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "zbirenbaum/copilot.lua",
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+          },
+        },
+      },
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        optional = true,
+        ft = function(_, ft)
+          vim.list_extend(ft, { "Avante" })
+        end,
+        opts = function(_, opts)
+          opts.file_types = vim.list_extend(opts.file_types or {}, { "Avante" })
+        end,
+      },
+      {
+        "folke/which-key.nvim",
+        optional = true,
+        opts = {
+          spec = {
+            { "<leader>a", group = "ai" },
+          },
+        },
+      },
+    },
     opts = {
       provider = "claude",
       auto_suggestions_provider = "copilot", -- Disabled
@@ -16,13 +55,22 @@ return {
       },
       system_prompt = function()
         local hub = require("mcphub").get_hub_instance()
-        return hub:get_active_servers_prompt()
+        if hub == nil then
+          return nil
+        else
+          return hub:get_active_servers_prompt()
+        end
       end,
       custom_tools = function()
         return {
           require("mcphub.extensions.avante").mcp_tool(),
         }
       end,
+      hints = { enabled = true },
+      file_selector = {
+        provider = "snacks",
+        provider_opts = {},
+      },
       vendors = {
         ["kenobi-llama"] = {
           __inherited_from = "openai",
@@ -47,34 +95,52 @@ return {
         },
       },
     },
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "zbirenbaum/copilot.lua",
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
+  },
+  {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    opts = {
+      input = { enabled = false },
+      select = { enabled = false },
+    },
+  },
+  {
+    "saghen/blink.compat",
+    lazy = true,
+    opts = {},
+    config = function()
+      require("cmp").ConfirmBehavior = {
+        Insert = "insert",
+        Replace = "replace",
+      }
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    lazy = true,
+    opts = {
+      sources = {
+        default = { "avante_commands", "avante_mentions", "avante_files" },
+        providers = {
+          avante_commands = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 90,
+            opts = {},
+          },
+          avante_files = {
+            name = "avante_files",
+            module = "blink.compat.source",
+            score_offset = 100,
+            opts = {},
+          },
+          avante_mentions = {
+            name = "avante_mentions",
+            module = "blink.compat.source",
+            score_offset = 1000,
+            opts = {},
           },
         },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "norg", "rmd", "org", "Avante" },
-        },
-        ft = { "markdown", "norg", "rmd", "org", "Avante" },
       },
     },
   },
